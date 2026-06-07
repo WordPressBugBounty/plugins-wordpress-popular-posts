e3c980b37d05439b7082a9ba8ff74ee45afc3815
Sean Murphy <sean@iamseanmurphy.com>
2026-06-07 19:44:39 +0000

    7.4.0

--- wordpress-popular-posts/src/Settings.php
+++ wordpress-popular-posts/src/Settings.php
@@ -100,7 +100,6 @@ class Settings {
                 'y_scale' => false
             ],
             'tools' => [
-                'experimental' => false,
                 'ajax' => true,
                 'css' => true,
                 'link' => [
@@ -116,7 +115,7 @@ class Settings {
                 ],
                 'log' => [
                     'level' => 1,
-                    'limit' => 0,
+                    'limit' => 1,
                     'expires_after' => 180
                 ],
                 'cache' => [
@@ -129,6 +128,10 @@ class Settings {
                 'sampling' => [
                     'active' => false,
                     'rate' => 100
+                ],
+                'views_column' => [
+                    'active' => false,
+                    'post_types' => 'post'
                 ]
             ]
         ]
